5be67cf2ebb9a8e8107c79dddb8cb2f0fc395827
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-06-11 02:52:24 +0800

    chore(nix): bump version to `0.3.1`

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
   outputs = { self, nixpkgs, flake-utils }:
     let
       name = "norden";
-      version = "0.3.0";
+      version = "0.3.1";
     in
     (flake-utils.lib.eachDefaultSystem
       (system:
